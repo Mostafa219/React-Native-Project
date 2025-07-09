7b32295e50426cdf1b13c5b0d89c9023aea6d227
Omnia Mohamed <omniamuhamed9191@gmail.com>
2025-07-10 01:04:47 +0300

    update film card

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,11 +1,14 @@
 import { Text, View } from "react-native";
 import React, { Component } from "react";
+import FilmCard from "../../components/FilmCard/Filmcard";
+import TrendMovie from "../../components/MovieCardTrend/MovieCardTrend.jsx";
 
 export default class Home extends Component {
   render() {
     return (
       <View>
-        <Text>Home</Text>
+        <TrendMovie/>
+        <FilmCard/>
       </View>
     );
   }
