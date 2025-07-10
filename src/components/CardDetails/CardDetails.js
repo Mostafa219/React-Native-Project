20868ac18e347dedc22b431a5f9a2d5b996b7195
mahmoudEbeid2 <mahmoud.ebeid2002@gmail.com>
2025-07-10 05:49:05 +0300

    solve ditales design

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -1,116 +1,106 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Entypo from "@expo/vector-icons/Entypo";
 
-const CardDetails = ({movie}) => {
-     const [isFav, setIsFav] = useState(false);
-    const {
+const CardDetails = ({ movie }) => {
+  const [isFav, setIsFav] = useState(false);
+  const {
     title,
     overview,
     genres,
     vote_average,
-    credits,
-    videos,
-    recommendations,
-    similar,
-    production_companies,
-    poster_path,
-    budget,
     release_date,
     runtime,
-    revenue,
-    vote_count,
     original_language,
   } = movie;
 
   return (
     <View style={styles.detailsContainer}>
-          <View style={[styles.row, styles.borderBottom]}>
-            <View>
-              <Text style={styles.title}>{movie.title}</Text>
-              {/* <Text style={styles.title}>EVIL DEAD RISE</Text> */}
-              <View style={styles.rating}>
-                {[...Array(5)].map((_, i) => (
-                  <AntDesign
-                    key={i}
-                    name="star"
-                    size={20}
-                    color={i < vote_average /2? "#fff" : "black"}
-                    style={styles.iconStar}
-                  />
-                  
-                ))}
-                <Text style={styles.rate}>{Math.floor(vote_average)}</Text>
-              </View>
-            </View>
-           
-            <TouchableOpacity
-              style={styles.favIconContainer}
-              onPress={() => setIsFav(!isFav)}
-            >
-              <Entypo
-                name="heart"
-                size={36}
-                style={isFav ? styles.activeFavIcon : styles.favIcon}
+      <View style={[styles.row, styles.borderBottom]}>
+        <View>
+          <Text style={styles.title}>{title}</Text>
+          <View style={styles.rating}>
+            {[...Array(5)].map((_, i) => (
+              <AntDesign
+                key={i}
+                name="star"
+                size={20}
+                color={i < vote_average / 2 ? "#fff" : "black"}
+                style={styles.iconStar}
               />
-            </TouchableOpacity>
-          </View>
-
-          <View style={styles.row}>
-            <View>
-              <Text style={styles.text}>lanuage: </Text>
-              <Text style={styles.text}>
-                {original_language == "en"
-                  ? "English"
-                  : original_language == "fr"
-                  ? "French"
-                  : original_language}{" "}
-              </Text>
-            </View>
-            <View>
-              <Text style={styles.text}>Duration: </Text>
-              <Text style={styles.text}>{runtime} Min </Text>
-            </View>
-            <View>
-              <Text style={styles.text}>Release date </Text>
-              <Text style={styles.text}>{release_date} </Text>
-            </View>
-          </View>
-          <View style={styles.borderBottom}>
-            <Text style={styles.text}>Categories</Text>
-            <View style={styles.category}>
-              {genres?.map((genre, index) => (
-                <Text style={styles.categoryText} key={index}>
-                  {genre.name}
-                </Text>
-              ))}
-            </View>
-          </View>
-          {/* description */}
-          <View>
-            <Text style={styles.description}>Description</Text>
-            <Text style={styles.text}>{overview}</Text>
+            ))}
+            <Text style={styles.rate}>{Math.floor(vote_average)}</Text>
           </View>
         </View>
-  )
-}
 
-export default CardDetails
+        <TouchableOpacity
+          style={styles.favIconContainer}
+          onPress={() => setIsFav(!isFav)}
+        >
+          <Entypo
+            name="heart"
+            size={36}
+            style={isFav ? styles.activeFavIcon : styles.favIcon}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.row}>
+        <View>
+          <Text style={styles.text}>Language:</Text>
+          <Text style={styles.text}>
+            {original_language === "en"
+              ? "English"
+              : original_language === "fr"
+              ? "French"
+              : original_language}
+          </Text>
+        </View>
+        <View>
+          <Text style={styles.text}>Duration:</Text>
+          <Text style={styles.text}>{runtime} Min</Text>
+        </View>
+        <View>
+          <Text style={styles.text}>Release Date:</Text>
+          <Text style={styles.text}>{release_date}</Text>
+        </View>
+      </View>
+
+      <View style={styles.borderBottom}>
+        <Text style={styles.text}>Categories</Text>
+        <View style={styles.category}>
+          {genres?.map((genre, index) => (
+            <Text style={styles.categoryText} key={index}>
+              {genre.name}
+            </Text>
+          ))}
+        </View>
+      </View>
+
+      <View>
+        <Text style={styles.description}>Description</Text>
+        <Text style={styles.text}>{overview}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default CardDetails;
 
 const styles = StyleSheet.create({
   detailsContainer: {
     paddingTop: 25,
-    padding: 15,
-    marginHorizontal: "auto",
+    padding: 20,
     position: "absolute",
     bottom: "-40%",
-    left: "50%",
-    transform: [{ translateX: "-50%" }],
-    width: 380,
+    width: 340,
     borderRadius: 50,
     backgroundColor: "#1E1E1E",
     zIndex: 3,
+    alignSelf: "center",
+    transform: [{ translateX: -195 }],
+    left: "50%",
   },
   row: {
     flexDirection: "row",
@@ -141,8 +131,8 @@ const styles = StyleSheet.create({
   },
   category: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    flexWrap: "wrap",
+    gap: 6,
     marginVertical: 10,
   },
   categoryText: {
@@ -150,8 +140,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: "center",
     backgroundColor: "#c55",
-    padding: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
     borderRadius: 20,
+    marginRight: 6,
+    marginBottom: 6,
   },
   favIconContainer: {
     backgroundColor: "#2E2E2E",
@@ -167,7 +160,8 @@ const styles = StyleSheet.create({
   activeFavIcon: {
     fontSize: 20,
     color: "#c33",
-  },rating: {
+  },
+  rating: {
     flexDirection: "row",
     alignItems: "center",
     marginVertical: 10,
@@ -179,5 +173,6 @@ const styles = StyleSheet.create({
     marginLeft: 8,
     fontSize: 16,
     fontWeight: "bold",
-    color: "#333",
-  },})
+    color: "#ccc",
+  },
+});
